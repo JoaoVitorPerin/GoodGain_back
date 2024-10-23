12cb35511e2c68ff4d9c0db5c5de683ca1103d2c
João Vitor Perin <joao.perin@inteliger.com.br>
2024-10-23 13:06:01 -0300

    add arquvios servidor

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goodgain_back.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GoodGain.settings')
 
 application = get_wsgi_application()
